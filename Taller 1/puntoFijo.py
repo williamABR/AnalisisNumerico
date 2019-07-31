1924173f00990941e50653700dcb20d2e632eade
William Baquero <williambaqueror@gmail.com>
2019-07-31 09:01:44 -0500

    .

--- Taller 1/puntoFijo.py
+++ Taller 1/puntoFijo.py
@@ -4,11 +4,11 @@ def g(x):
     return (math.pow(x,3)-1)/-5
 
 
-x = 2.4
+x = 8
 y = round(g(x), 3)
 w = 0
 
-while math.fabs(y-x) > 1*math.pow(10,-8):
+while math.fabs(y-x) > math.pow(10,-8):
     x = y
     y = round(g(x), 3)
     print math.fabs(y-x)
